311d23f877864be6905c9d36c7e9751989fdeca4
VDAY-CRTV-PC10\Aldrin.Tapia <johnaldrin.tapia@volenday.com>
2018-08-02 18:48:01 +0800

    [project-dashboard] : update counting of issues and on track

--- model/project.js
+++ model/project.js
@@ -62,17 +62,13 @@ var getDataCount = exports.getDataCount = ( tablename, data, advance , cb ) => {
     let db = global.initDB();
     let params = [];
     
-    let query = `SELECT 
-                        typeId,
-                        Active,
-                        type,
-                        linkType 
-                FROM (SELECT 
-                        typeId,
-                        SUM(IF(isActive="1",1,0)) as Active 
-                      FROM project 
-                      GROUP BY typeId) AS tb 
-                      LEFT JOIN type ON tb.typeId = type.id`;
+    let query = `SELECT tb.typeId,Active,type,linkType,taskStatus.Issues,taskStatus.OnTrack  
+                        FROM (select typeId,sum(IF(isActive="1",1,0)) as Active from project 
+                        GROUP BY typeId) as tb 
+                            LEFT JOIN type ON tb.typeId = type.id
+                            LEFT JOIN (SELECT typeId,sum(Issues) as Issues,sum(OnTrack) as OnTrack FROM (SELECT typeId,projectId,IF(Issues>0,1,0) as Issues,IF(OnTrack>0,1,0) as OnTrack FROM project
+                            LEFT JOIN (SELECT projectId,SUM(IF(dueDate>NOW(),1,0)) as OnTrack, SUM(IF(dueDate<NOW(),1,0)) as Issues FROM task) as tbTask ON project.id = tbTask.projectId) as tbpt GROUP BY typeId) as taskStatus ON tb.typeId = taskStatus.typeId;
+`;
 
     db.query(
         query,
--- src/container/pageNotAvailable.js
+++ src/container/pageNotAvailable.js
@@ -2,7 +2,7 @@ import React from "react"
 import ReactDOM from "react-dom"
 import { Provider } from "react-redux"
 
-import Component from  "../components/pageNotFound"
+import Component from  "../components/pageNotAvailable"
 import Socket from  "../components/socket"
 
 import store from "../store"
