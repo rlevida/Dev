4cb9b7bdbc3505c590d09b96031ff50d5bcaa308
randur <randur.duran@volenday.com>
2018-08-07 13:29:27 +0800

    [document] : update trash document list

--- src/components/trash/list.js
+++ src/components/trash/list.js
@@ -11,7 +11,10 @@ import { connect } from "react-redux"
         socket: store.socket.container,
         document: store.document,
         loggedUser: store.loggedUser,
-        users : store.users
+        users : store.users,
+        starred : store.starred,
+        global : store.global,
+        workstream : store.workstream
     }
 })
 export default class List extends React.Component {
@@ -26,58 +29,61 @@ export default class List extends React.Component {
 
     componentWillMount() {
         let { socket } = this.props
-        socket.emit("GET_DOCUMENT_LIST", { filter : { isDeleted: 1 , linkId: project , linktype: "project" }});
-        socket.emit("GET_STATUS_LIST",{});
-        socket.emit("GET_TYPE_LIST",{});
-        socket.emit("GET_USER_LIST",{});
+            socket.emit("GET_DOCUMENT_LIST", { filter : { isDeleted: 1 , linkId: project , linkType: "project" }});
+            socket.emit("GET_APPLICATION_SELECT_LIST",{ selectName : "tagList" , filter : { tagType : "document" } })
+            socket.emit("GET_WORKSTREAM_LIST", {filter:{projectId:project}});
+            socket.emit("GET_STATUS_LIST",{});
+            socket.emit("GET_TYPE_LIST",{});
+            socket.emit("GET_USER_LIST",{});
     }
 
     updateActiveStatus(id,active){
         let { socket, dispatch } = this.props;
-        dispatch({type:"SET_DOCUMENT_STATUS",record:{id:id,status:(active==1)?0:1}})
-        socket.emit("SAVE_OR_UPDATE_DOCUMENT",{data : {id:id,active:(active==1)?0:1}})
+            dispatch({type:"SET_DOCUMENT_STATUS",record:{id:id,status:(active==1)?0:1}})
+            socket.emit("SAVE_OR_UPDATE_DOCUMENT",{data : {id:id,active:(active==1)?0:1}})
     }
 
     deleteData(id){
         let { socket } = this.props;
-        if(confirm("Do you really want to delete this record?")){
-            socket.emit("DELETE_DOCUMENT",{id:id})
-        }
+            if(confirm("Do you really want to delete this record?")){
+                socket.emit("DELETE_DOCUMENT",{id:id})
+            }
     }
 
     saveData(){
         let { socket } = this.props
         let { tempData } = this.state
-        socket.emit("SAVE_OR_UPDATE_DOCUMENT", { data:tempData , loggedUser : "" })
+            socket.emit("SAVE_OR_UPDATE_DOCUMENT", { data:tempData , loggedUser : "" })
     }
 
     handleIsCompleted(data,value){
         let { socket , document } = this.props;
-        socket.emit("SAVE_OR_UPDATE_DOCUMENT", { data : { id: data.id , isCompleted : value ? 0 : 1  }})
+            socket.emit("SAVE_OR_UPDATE_DOCUMENT", { data : { id: data.id , isCompleted : value ? 0 : 1  }})
     }
 
     render() {
-        let { document, dispatch, socket ,users } = this.props;
-        let data = [] , tempData = [] , documentList = { deleted : []};
+        let { document, dispatch, users , starred , global , workstream } = this.props;
+        let tagList = [];
 
-        if( document.List.length > 0 ){
+            if(typeof global.SelectList.tagList != "undefined"){
+                global.SelectList.tagList.map( t => {
+                    if(workstream.List.filter( w => { return w.id == t.linkId}).length > 0 ){
+                    let workstreamName =  workstream.List.filter( w => { return w.id == t.linkId})[0].workstream
+                    tagList.push({ tagTypeId :t.tagTypeId  , name : workstreamName , linkId : t.linkId })
+                    }
+                })
+            }
 
-            document.List.filter( e =>{
-                if( e.isDeleted ){
-                    documentList.deleted.push(e)
-                }
-            })
-        }
-        
         return <div>
                 <HeaderButtonContainer  withMargin={true}>
                     <li class="btn btn-info" onClick={(e)=>dispatch({type:"SET_DOCUMENT_FORM_ACTIVE", FormActive: "Form" })} >
                         <span>New Document</span>
                     </li>
                 </HeaderButtonContainer>
-                <table id="dataTable" class="table responsive-table">
+                <table id="dataTable" class="table responsive-table table-bordered document-table">
                     <tbody>
                         <tr>
+                            <th></th>
                             <th></th>
                             <th>Name</th>
                             <th>Uploaded</th>
@@ -92,25 +98,28 @@ export default class List extends React.Component {
                             </tr>
                         }
                         {
-                            documentList.deleted.map((data, index) => {
+                            document.List.map((data, index) => {
                                 return <tr key={index}>
-                                         <td> <input type="checkbox" onChange={ () => this.handleIsCompleted(data , data.isCompleted ) } checked={ data.isCompleted }/></td>
+                                        <td> <input type="checkbox" onChange={ () => this.handleIsCompleted(data , data.isCompleted ) } checked={ data.isCompleted }/></td>
+                                        <td>{
+                                            starred.List.filter( s => { return s.linkId == data.id }).length > 0 
+                                                ? <span class="glyphicon glyphicon-star"></span>
+                                                    : <span class="glyphicon glyphicon-star-empty"></span> 
+                                            }
+                                        </td>
                                         <td>{data.origin}</td>
                                         <td>{ moment(data.dateAdded).format('L') }</td>
                                         <td>{ (users.List .length > 0) ? users.List.filter( f => { return f.id == data.uploadedBy })[0].emailAddress : ""}</td>
-                                        <td> 
-                                            { ( data.tags != "" && data.tags != null ) &&
-                                                JSON.parse(data.tags).map((tag,tagIndex) =>{
-                                                    return <span key={tagIndex} class="label label-primary" style={{margin:"5px"}}>{tag.label}</span>
+                                        <td>{ (tagList.length > 0) &&
+                                                tagList.map((t,tIndex) =>{
+                                                    if(t.tagTypeId == data.id){
+                                                        return <span key={tIndex} class="label label-primary" style={{margin:"5px"}}>{t.name}</span>
+                                                    }
                                                 })
                                             }
                                         </td>
-                                        {/* <td class="text-center">
-                                            <a href="javascript:void(0);" data-tip="Undo"
-                                                onClick={e => this.undoData(data.id)}
-                                                class={ data.allowedDelete==0 ? 'hide' : 'btn btn-primary btn-sm ml10' }>
-                                                <span class="glyphicon glyphicon-refresh"></span></a>
-                                        </td> */}
+                                        <td>
+                                        </td>
                                     </tr>
                             })
                         }
