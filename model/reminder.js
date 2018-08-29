8376556c4352b38bc1c562981fa1f19bdc24f188
VDAY-CRTV-PC10\Aldrin.Tapia <johnaldrin.tapia@volenday.com>
2018-08-29 16:20:53 +0800

    [reminder-feature] : add reminder sql and model

---
+++ model/reminder.js
@@ -0,0 +1,43 @@
+var field = exports.field = {
+
+    /**
+     *  Id (Primary Key)
+     */
+    'id' : {type : 'bigint' , access : "public" },
+
+    /**
+     * reminderDetail (VARCHAR(50))
+     */
+    'reminderDetail' : {type : 'string' , access : "public" },
+
+     /**
+     * userId (BIGINT)
+     */
+    'userId' : {type : 'int' , access : "public" },
+
+    /**
+     * taskId (BIGINT)
+     */
+    'taskId' : {type : 'int' , access : "public" },
+
+    /**
+     * seen (TINYINT)
+     */
+    'seen' : {type : 'int' , access : "public" },
+    
+    /**
+     * dateAdded (DATETIME)
+     */
+    'dateAdded' : {type : 'date' , access : "public" },
+
+    /**
+     * dateUpdated (TIMESTAMP)
+     */
+    'dateUpdated' :  {type : 'date' , access : "public" }
+}
+
+var { getData, putData, postData, deleteData } = require("./index");
+exports.getData = getData;
+exports.putData = putData;
+exports.postData = postData;
+exports.deleteData = deleteData;
---
+++ sql/reminder.sql
@@ -0,0 +1,12 @@
+DROP TABLE IF EXISTS `reminder`;
+
+CREATE TABLE `reminder` (
+    `id` BIGINT UNSIGNED AUTO_INCREMENT,
+    `reminderDetail` VARCHAR(50),
+    `userId` BIGINT,
+    `taskId` BIGINT,
+    `seen` TINYINT(1) DEFAULT '0',
+    `dateAdded` DATETIME,
+    `dateUpdated` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    PRIMARY KEY(`id`)
+) ENGINE=INNODB;
