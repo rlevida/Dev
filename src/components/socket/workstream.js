3ef912c84ea3e2facac3c2bdd7302d999871a8f9
andrienpecson <andrien.pecson@volenday.com>
2018-09-10 17:21:54 +0800

    Fixed deleting of workstream to update count.

--- src/components/socket/workstream.js
+++ src/components/socket/workstream.js
@@ -32,7 +32,7 @@ export default class Socket extends React.Component {
         })
 
         socket.on("FRONT_WORKSTREAM_ADD", (data) => {
-            dispatch({ type: "SET_WORKSTREAM_SELECTED", Selected: data[0] })
+            dispatch({ type: "SET_WORKSTREAM_SELECTED", Selected: {} })
             dispatch({ type: "SET_WORKSTREAM_FORM_ACTIVE", FormActive: "List" })
         })
 
@@ -43,6 +43,7 @@ export default class Socket extends React.Component {
         })
 
         socket.on("FRONT_WORKSTREAM_DELETED", (data) => {
+            socket.emit("GET_WORKSTREAM_COUNT_LIST", { filter: { projectId: data.projectId } });
             dispatch({ type: "REMOVE_DELETED_WORKSTREAM_LIST", id: data.id, List: this.props.workstream.List })
             showToast("success", "Workstream already deleted.")
         })
