2bcdd5576ee9eabf6765e041bf19b97b058e79f8
randur <randur.duran@volenday.com>
2018-10-01 10:02:56 +0800

    [document-upload]  : fixed document library upload

--- controller/document.js
+++ controller/document.js
@@ -31,6 +31,56 @@ exports.post = {
                 cb({ status:false, error:res.error })
             }
         })
+    },
+    upload : (req,cb) => {
+        var formidable = global.initRequire("formidable"),
+        modalFunc = global.initModelFunc(),
+        func = global.initFunc();
+
+        var form = new formidable.IncomingForm();
+        var filenameList = [];
+        var files = []
+        form.multiples = true;
+
+
+        let type = (typeof req.query.type != "undefined")?req.query.type:"others";
+        let uploadType = (typeof req.query.uploadType != "undefined")?req.query.uploadType:"";
+        let uploaded = false;
+        // every time a file has been uploaded successfully copy to AWS
+        
+        files.push( new Promise((resolve,reject) =>{
+            form.on('file', function(field, file) {
+            var date = new Date();
+            var Id = func.generatePassword(date.getTime()+file.name,"attachment");
+            var filename = file.name + "_" + Id + "." + func.getFilePathExtension(file.name);
+            // var filename = file.name;
+            if(uploadType == "form"){
+                filenameList.push({filename:filename,origin:file.name,Id:Id});
+            }else{
+                filenameList.push(filename);
+            }
+                // func.uploadFile({file : file, form : type, filename : filename},response =>{
+                //     if(response.Message == 'Success'){
+                //         resolve(filenameList)
+                //     }
+                // });
+            });
+        }))
+
+
+        Promise.all(files).then( e =>{
+            // cb({ status:true, data: e[0] })
+        })
+        // log any errors that occur
+        form.on('error', function(err) {
+            console.log('An error has occured: \n' + err);
+        });
+        // once all the files have been uploaded, send a response to the client
+        form.on('end', function() {
+            cb({ status:true, data: filenameList })
+        });
+        // parse the incoming request containing the form data
+        form.parse(req);
     }
 }
 
