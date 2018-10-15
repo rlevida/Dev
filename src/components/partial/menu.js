bd9c68ad4ffa36150d75541203f63a9e5e46c6d1
randur <randur.duran@volenday.com>
2018-10-15 10:35:29 +0800

    [task-componet] : fixed task menu label

--- src/components/partial/menu.js
+++ src/components/partial/menu.js
@@ -86,7 +86,7 @@ export default class Component extends React.Component {
                     </li>
                     <li class={page == "project" && subpage=="task"?"active":""}>
                         <a href={`/project/${project}/task`}>
-                        <i class="fa fa-tasks"></i>&nbsp; Tasks 1</a> 
+                        <i class="fa fa-tasks"></i>&nbsp; Tasks </a> 
                     </li>
                     <li class="has-submenu">
                         <a href={"/project/"+project} class="dropdown-toggle has-submenu" data-toggle="dropdown" aria-expanded="true">
