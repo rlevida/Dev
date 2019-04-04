dbce92f2bde72f9f6cc412964c11598790051353
andrien.pecson.volenday <andrien.pecson@volenday.com>
2019-04-04 16:25:00 +0800

    Check socket

--- serverSocket/index.js
+++ serverSocket/index.js
@@ -1,3 +1,5 @@
 module.exports = function (io) {
-    io.sockets.on("connection", (socket) => {});
+    io.sockets.on("connection", (socket) => {
+        console.log("connected to socket.");
+    });
 };
