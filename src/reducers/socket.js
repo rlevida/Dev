c7993cef1803def7855ee27a75edf859193572d1
andrien.pecson.volenday <andrien.pecson@volenday.com>
2019-04-04 16:50:10 +0800

    Fixing socket issue in the test environment.

--- src/reducers/socket.js
+++ src/reducers/socket.js
@@ -1,6 +1,6 @@
 
 import io from 'socket.io-client';
-const socket = io();
+const socket = io('ws://ui-cloudcfo.volenday.com', {transports: ['websocket']});
 
 export default function reducer(state={
         container : socket
