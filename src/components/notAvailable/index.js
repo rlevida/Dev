f60c6b2895846c89f0d71438ff06fce69e7105aa
andrien.pecson.volenday <andrien.pecson@volenday.com>
2019-04-06 22:35:03 +0800

    Add: Icon on not available.

--- src/components/notAvailable/index.js
+++ src/components/notAvailable/index.js
@@ -41,6 +41,7 @@ class Component extends React.Component {
             <div class="row">
                 <div class="col-lg-12 col-md-12 col-sm-12">
                     <div class="no-project">
+                        <i class="fa fa-exclamation" aria-hidden="true"></i>
                         <span>Page Not Available</span>
                     </div>
                 </div>
