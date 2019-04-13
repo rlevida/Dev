eeb455c2caaa8ca805eed8ce7187a2e24d082c0c
andrien.pecson.volenday <andrien.pecson@volenday.com>
2019-04-13 16:27:09 +0800

    Fix: Backup of database.

--- backup.js
+++ backup.js
@@ -1,42 +1,31 @@
-var schedule = require('node-schedule')
-var j = schedule.scheduleJob('0 0 0 * * *', () => {
-  var mysqlDump = require('mysqldump');
-  var moment = require('moment');
-  var database = require('./config/database');
-  var dbConfig = database.connectionCredentials;
-  var currentName = moment().format("YYYYMMDDHH:mm:ss");
-  dbConfig['dropTable'] = true;
-  dbConfig['ifNotExist'] = true;
-  dbConfig['dest'] = './temp/cloudcfo.sql';
-  mysqlDump(dbConfig, function (response, dump) {
-     var AWS = global.initAWS();
-    
-    var tar = require("tar"),
-        fs = require("graceful-fs")
-    
-    var writeStream  = tar.c(
-        {
-            gzip: "gzip",
-        },
-        ["./temp/cloudcfo.sql"]
-    ).pipe(fs.createWriteStream('./temp/backup.tgz'))
-    writeStream.on('finish', () => {
-        var fileStream = fs.createReadStream('./temp/backup.tgz');
-        fileStream.on('error', function(err){
-            console.log("Error in creating file stream. [" + err + "]");
+const schedule = require('node-schedule');
+const moment = require('moment');
+const database = require('./config/database');
+
+schedule.scheduleJob('0 0 0 * * *', () => {
+    const config = database.connectionCredentials;
+    const currentName = moment(new Date()).utc().tz("Asia/Manila").format("YYYYMMDDHH:mm:ss");
+    const func = global.initFunc();
+    config['dest'] = './temp/' + currentName + '.sql';
+    config['getDump'] = true;
+
+    mysqlDump(config, function (response, dump) {
+        func.uploadFile({
+            file: dump,
+            form: "backup",
+            filename: currentName
+        }, response => {
+            empty('./temp', false, (o) => {
+                if (response.Message != 'Success') {
+                    var mailOptions = {
+                        from: 'noreply<mobbizapps12345@gmail.com>',
+                        to: 'andrien.pecson@volenday.com',
+                        subject: "CloudCFO Database Backup",
+                        html: "Error in database backup for the CloudCFO website."
+                    };
+                    global.emailtransport(mailOptions)
+                }
+            });
         });
-        fileStream.on('open', function(){
-            var s3 = new AWS.S3();
-                s3.putObject({
-                    Bucket: global.AWSBucket,
-                    Key: "backup/"+global.environment+"/cloudcfo"+currentName+".tgz",
-                    Body: fileStream,
-                    ContentType : "tgz"
-                }, function(err){
-                    console.log("Error in Uploading to AWS. [" + err + "]");
-                });
-        });
-    })
-      
-  });
+    });
 });
