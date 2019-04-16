ff209dd49d417c369fe18bf47b2efd597021f129
andrien.pecson.volenday <andrien.pecson@volenday.com>
2019-04-16 08:41:08 +0800

    Fix: Backup of database.

--- backup.js
+++ backup.js
@@ -2,28 +2,32 @@ const schedule = require('node-schedule');
 const moment = require('moment');
 const database = require('./config/database');
 const mysqlDump = require('mysqldump');
+const AWS = global.initAWS();
 
 schedule.scheduleJob('0 0 0 * * *', () => {
     const config = database.connectionCredentials;
-    const currentName = moment(new Date()).utc().tz("Asia/Manila").format("YYYYMMDDHH:mm:ss");
-    const func = global.initFunc();
+    const currentName = moment(new Date()).utc().tz("Asia/Manila").format("YYYYMMDDHHmmss");
     config['dest'] = './temp/' + currentName + '.sql';
     config['getDump'] = true;
 
     mysqlDump(config, function (response, dump) {
-        func.uploadFile({
-            file: dump,
-            form: "backup",
-            filename: currentName
-        }, response => {
+        const s3 = new AWS.S3();
+        const environment = global.environment || "development";
+
+        s3.putObject({
+            Bucket: global.AWSBucket,
+            Key: environment + "/backup/" + currentName + ".sql",
+            Body: dump,
+            ContentType: "sql"
+        }, function (err) {
             empty('./temp', false, (o) => {
                 let mailOptions = {
                     from: 'noreply<mobbizapps12345@gmail.com>',
                     to: 'andrien.pecson@volenday.com',
                     subject: "CloudCFO Database Backup"
                 };
-                if (response.Message != 'Success') {
-                    mailOptions['html'] = "Error in database backup for the CloudCFO website."
+                if (err != null) {
+                    mailOptions['html'] = "Error in database backup for the CloudCFO website. " + err;
                 } else {
                     mailOptions['html'] = "Database successfully backup for the CloudCFO website."
                 }
