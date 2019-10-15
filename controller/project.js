9c997b4d3a18c70fecf67883d962c623046bda62
Randur <randur.duran@mobbizsolutions.com>
2019-10-15 13:01:05 +0800

    fixed project picture upload

--- controller/project.js
+++ controller/project.js
@@ -1345,7 +1345,7 @@ exports.post = {
     },
     upload: async (req, cb) => {
         if (req.query.projectId) {
-            const hasAccess = await projectAuth({ token: req.cookies["app.sid"], projectId: req.query.projectId, action: "post" });
+            const hasAccess = await projectAuth({ user: req.user, projectId: req.query.projectId, action: "post" });
             if (!hasAccess) {
                 cb({ status: false, error: "Unauthorized Access" });
                 return;
