9a59bdab4f6d52e3e886ff1f112a63984dba77dc
Randur <randur.duran@mobbizsolutions.com>
2019-11-06 15:15:15 +0800

    added user rights

--- src/components/main/app.js
+++ src/components/main/app.js
@@ -316,13 +316,6 @@ class Main extends React.Component {
                 component: inactiveProject,
                 show_menu: false
             },
-            {
-                label: "Terms & Conditions",
-                icon: "fa-info",
-                path_name: "terms-and-conditions",
-                component: TermsAndConditions,
-                show_menu: true
-            }
         ];
         const dropdownAddLinks = [{ id: "task", label: "Task" }, { id: "project", label: "Project" }, { id: "user", label: "User" }, { id: "team", label: "Team" }];
         const projectMenu = [
@@ -339,11 +332,23 @@ class Main extends React.Component {
                 return path_name != "users-and-team";
             });
         }
+
+        if(loggedUser.data.userRole === 1){
+            pages.push({
+                label: "Terms & Conditions",
+                icon: "fa-info",
+                path_name: "terms-and-conditions",
+                component: TermsAndConditions,
+                show_menu: true
+            })
+        }
+
         const currentPath = this.props.location.pathname;
         const parentPath = currentPath.split("/")[1];
         const currentPage = _.find(pages, page => {
             return page.path_name == parentPath;
         });
+        
         const getProjectDetailsPath = currentPath.split("/");
         const showProjectMenu = getProjectDetailsPath[2] == project.Selected.id && typeof project.Selected.id != "undefined";
         const currentProjectPage = typeof getProjectDetailsPath[3] == "undefined" ? "dashboard" : getProjectDetailsPath[3];
