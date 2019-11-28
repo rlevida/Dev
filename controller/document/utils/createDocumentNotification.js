4c323c33555d444941786a973a32a54b657f0b05
Randur <randur.duran@mobbizsolutions.com>
2019-11-28 13:04:48 +0800

    [UPDATE]: update document notification script

--- controller/document/utils/createDocumentNotification.js
+++ controller/document/utils/createDocumentNotification.js
@@ -3,17 +3,14 @@ const models = require("../../../modelORM");
 const Sequelize = require("sequelize");
 const { uniqBy, flatten } = require("lodash")
 const Op = Sequelize.Op;
-const { Tasks, Members, Users, UsersNotificationSetting, Notification } = models;
-const { notificationIncludes } = require("../../includes/notification");
-const moment = require("moment");
+const { Tasks, Members, Users, UsersNotificationSetting } = models;
 
-// const notificationTemplate = global.notificationEmailTemplate();
-// const func = global.initFunc();
+const sendNotification = require("../../sendNotification");
 
 module.exports = async (params) => {
     const { documents, tagWorkstream, usersId, projectId, req } = { ...params }
     const workstreamIds = tagWorkstream.map(({ value }) => { return value });
-    let taskNotificationBulkCreateData = [], workstreamNotificationBulkCreateData = [];
+    // let taskNotificationBulkCreateData = [], workstreamNotificationBulkCreateData = [];
 
     /* Task Members */
 
@@ -48,12 +45,12 @@ module.exports = async (params) => {
         documents.forEach((documentObj) => {
             const taskUsers = taskResponse.toJSON().task_members.map(({ user }) => {
                 return {
+                    notificationSocket: req.app.parent.io,
+                    sender: req.user,
+                    receiver: user.id,
+                    notificationType: "fileNewUpload",
+                    notificationData: { document: documentObj },
                     projectId, workstreamId,
-                    documentId: documentObj.id,
-                    usersId: user.id,
-                    createdBy: usersId,
-                    type: "fileNewUpload",
-                    message: "upload a new file",
                 }
             });
             taskMemberNotification = [...taskMemberNotification, ...taskUsers]
@@ -61,7 +58,11 @@ module.exports = async (params) => {
         return taskMemberNotification;
     })
 
-    taskNotificationBulkCreateData = uniqBy(flatten(findTaskMembersResult), "usersId");
+    documentTaskMemberNotificationData = uniqBy(flatten(findTaskMembersResult), "receiver");
+
+    documentTaskMemberNotificationData.forEach(async notificationObj => {
+        await sendNotification(notificationObj);
+    })
 
     /* Workstream Members */
 
@@ -87,66 +88,23 @@ module.exports = async (params) => {
 
         let taskWorkstreamMmbemrNotification = documents.map((documentObj) => {
             return {
+                notificationSocket: req.app.parent.io,
+                sender: req.user,
+                receiver: user.id,
+                notificationType: "fileNewUpload",
+                notificationData: { document: documentObj },
                 projectId: projectId,
                 workstreamId: linkId,
-                documentId: documentObj.id,
-                usersId: user.id,
-                createdBy: usersId,
-                type: "fileNewUpload",
-                message: "upload a new file",
             };
         })
         return taskWorkstreamMmbemrNotification
     })
 
-    workstreamNotificationBulkCreateData = uniqBy(flatten(findWorkstreamMemberResult), "usersId");
+    workstreamDocumentNotificationData = uniqBy(flatten(findWorkstreamMemberResult), "receiver");
 
-    /* Notification */
-    const notificationBulkCreateData = uniqBy([...workstreamNotificationBulkCreateData, ...taskNotificationBulkCreateData], "usersId")
-
-    const notificationResult = await Notification.bulkCreate(notificationBulkCreateData)
-        .map(notificationResponse => {
-            return notificationResponse.toJSON().id;
-        });
-
-    const findNotificationResult = await Notification.findAll({
-        where: { id: notificationResult },
-        include: notificationIncludes()
-    }).map(findNotificationResponse => {
-        req.app.parent.io.emit("FRONT_NOTIFICATION", {
-            ...findNotificationResponse.toJSON()
-        });
-        return {
-            ...findNotificationResponse.toJSON(),
-            user: findNotificationResponse.toJSON().to,
-            notification_setting: findNotificationResponse.toJSON().to.notification_setting[0]
-        };
+    workstreamDocumentNotificationData.forEach(async notificationObj => {
+        await sendNotification(notificationObj);
     })
 
-    findNotificationResult.forEach((notificationObj, ) => {
-        const { notification_setting, to, message } = { ...notificationObj }
-        const { receiveEmail } = { ...notification_setting }
-
-        if (receiveEmail) {
-
-            let html = "<p>" + message + "</p>";
-            html += '<p style="margin-bottom:0">Title: ' + message + "</p>";
-            html += "<p>Message:<br>" + message + "</p>";
-            html += `<p>Date:<br>${moment().format("LLL")}</p>`;
-
-            const mailOptions = {
-                from: '"no-reply" <no-reply@c_cfo.com>',
-                to: `${to.emailAddress}`,
-                subject: "[CLOUD-CFO]",
-                html: html
-            };
-
-            global.emailtransport(mailOptions);
-
-            // const htmlBody = notificationTemplate.fileUploadEmailNotification(notificationObj);
-            // const mailOptions = func.MailOptions({ to: to.emailAddress, subject: "New files were uploaded", html: htmlBody, });
-            // global.emailtransport(mailOptions);
-        }
-    })
     return;
 }
