14cbaf20d744bac885c8c79d814bb24bf3565213
Randur <randur.duran@mobbizsolutions.com>
2019-12-17 10:14:30 +0800

    [REMOVE]: remove reconnect attempt

--- serverSocket/index.js
+++ serverSocket/index.js
@@ -2,7 +2,4 @@ module.exports = function (io) {
     io.on("connection", (socket) => {
         console.log("connected to socket.");
     });
-    io.on('reconnect_attempt', (socket) => {
-        socket.io.opts.transports = ['polling', 'websocket'];
-    })
 };
