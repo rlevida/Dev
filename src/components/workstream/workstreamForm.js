134fc53f5427b57095a273bd46a9bff11d12e30f
Randur <randur.duran@mobbizsolutions.com>
2020-06-01 14:45:38 +0800

    Fixed inactive user menu, approver, task assigned list.

--- src/components/workstream/workstreamForm.js
+++ src/components/workstream/workstreamForm.js
@@ -48,7 +48,7 @@ export default class WorkstreamForm extends React.Component {
 
     fetchMemberList(options) {
         const { dispatch, project } = this.props;
-        let fetchUrl = `/api/project/getProjectMembers?page=1&linkId=${project.Selected.id}&linkType=project&project_type=${project.Selected.type.type}&memberType=responsible`;
+        let fetchUrl = `/api/project/getProjectMembers?page=1&linkId=${project.Selected.id}&linkType=project&project_type=${project.Selected.type.type}&memberType=responsible&isActive=1`;
 
         if (typeof options != "undefined" && options != "") {
             fetchUrl += `&memberName=${options}`;
